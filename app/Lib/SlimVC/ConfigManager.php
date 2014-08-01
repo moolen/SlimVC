0263f6772a6181264a15aa61ae527e8164ba7a57
Moritz Beller <mb@sopg.de>
2014-08-01 18:11:20 +0200

    ConfigManager now being cached if APC is enabled (saves about 9-25ms)

--- app/Lib/SlimVC/ConfigManager.php
+++ app/Lib/SlimVC/ConfigManager.php
@@ -7,9 +7,28 @@ use \App\Lib\SlimVC\PageTemplate as PageTemplate;
 
 class ConfigManager{
 
+	/**
+	 * default configuration directory
+	 * @var string
+	 */
 	protected $configDir = '../../Config';
+
+	/**
+	 * extension of config files
+	 * @var string
+	 */
 	protected $extension = '.php';
+
+	/**
+	 * holds a PageTemplate instance
+	 * @var [\App\Lib\SlimVC\PageTemplate]
+	 */
 	protected $PageTemplate;
+
+	/**
+	 * filenames which will be included
+	 * @var array
+	 */
 	protected $fileNames = array(
 		'application',
 		'images',
@@ -20,6 +39,22 @@ class ConfigManager{
 		'taxonomies'
 	);
 
+	/**
+	 * APC specific 
+	 * @var boolean
+	 */
+	protected $APC_ENABLED = false;
+
+	/**
+	 * APC namespace-prefix
+	 * @var string
+	 */
+	protected $APC_PREFIX = 'CONFIG_MANAGER_';
+
+	/**
+	 * default image size definition array
+	 * @var array
+	 */
 	protected $defaultImageSize = array(0,0,false);
 
 	/**
@@ -28,6 +63,9 @@ class ConfigManager{
 	 */
 	public function __construct( $parent ){
 		$this->parent = $parent;
+
+		$this->APC_ENABLED = extension_loaded('apc') && ini_get('apc.enabled');
+
 		$this->PageTemplate = new PageTemplate();
 		$this->readConfigDir();
 		$this->applyConfiguration();
@@ -40,14 +78,66 @@ class ConfigManager{
 	 * @return [void]
 	 */
 	protected function readConfigDir(){
-		foreach( $this->fileNames as $file ){
-			$path = trailingslashit(dirname(__FILE__)) . trailingslashit($this->configDir) . $file . $this->extension;
-			if( file_exists( $path ) ){
-				$this->$file = include $path;
+
+		// remove application conf from filenames
+		$strippedFilenames = array_diff($this->fileNames, array('application') );
+
+		// read app conf directly
+		$this->application = array_merge( $this->getApplicationDefaults(), $this->read('application') );
+
+		// the rest might be cached if available
+		foreach( $strippedFilenames as $file ){
+
+			// apc enabled ?
+			if( $this->APC_ENABLED ){
+				// fetch from cache if exists
+				if( (!apc_exists( $this->APC_PREFIX . 'FILE_' . $file )) && (true !== $this->application['debug']) ){
+					$content = $this->read($file);
+					apc_add($this->APC_PREFIX . 'FILE_' . $file, $content);
+					$this->$file = $content;
+				}else{
+					$this->$file = apc_fetch($this->APC_PREFIX . 'FILE_' . $file);
+				}
 			}else{
-				$this->$file = array();
+				$this->$file = $this->read($file);
 			}
 		}
+	}
+
+	/**
+	 * reads a file and returns its content
+	 * @param  [string] $file
+	 * @return [mixed]
+	 */
+	protected function read($file){
+		$path = trailingslashit(dirname(__FILE__)) . trailingslashit($this->configDir) . $file . $this->extension;
+		if( file_exists( $path ) ){
+			return include $path;
+		}else{
+			return array();
+		}
+	}
+
+	/**
+	 * application-wide default configuration
+	 * @var array
+	 */
+	protected function getApplicationDefaults(){
+		return array(
+			'debug' => true,
+			'namespace.controller' => '\\App\\Conrollers\\',
+			'slim' => array(
+				// env vars
+				'mode' => 'development',
+				'log.enabled' => true,
+				'log.writer' => new \App\Lib\SlimVC\Logger(),
+				'log.level' => \Slim\Log::DEBUG,
+
+				// view & templating
+				'view' => new \Slim\Views\Twig(),
+				'templates.path' => dirname(__FILE__) . '/../Views',
+			)
+		);
 	}
 
 	/**
